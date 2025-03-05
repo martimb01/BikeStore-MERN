813798f7b7610858aff1fa02e958ae0eea59d979
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-03-05 20:39:24 +0000

    Finished navbar component and working on CreatePage component

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -25,7 +25,7 @@ const NavBar = () => {
 					bgGradient={"linear(to-r, cyan.400, blue.500)"}
 					bgClip={"text"}
 				>
-					<Link to={"/"}>Product Store 🛒</Link>
+					<Link to={"/"}>Martim's Store 🛒</Link>
 				</Text>
 
                 <HStack spacing={2} alignItems={"center"}>
