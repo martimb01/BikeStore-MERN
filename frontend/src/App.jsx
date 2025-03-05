f14abbccda333a947d089fab0b5ee200c4b3b614
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-03-05 19:39:53 +0000

    Starting frontend work with chakra

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Button } from '@chakra-ui/react'
+
+function App() {
+  
+
+  return (
+    <Button>Heilo</Button>
+  )
+}
+
+export default App
