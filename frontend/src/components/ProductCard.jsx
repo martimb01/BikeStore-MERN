58b427ce10554781221f489e6d46596f444d644f
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-03-06 16:13:26 +0000

    Added deleteProduct function to global hook and incorporated into ProductCard componenet

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,7 +1,41 @@
-import { Box, useColorModeValue } from "@chakra-ui/react";
-
+import { Box, useColorModeValue, Image, Heading, Text, HStack, IconButton, useToast } from "@chakra-ui/react";
+import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
+import { useProductStore } from "../store/product";
 const ProductCard = ({ product }) => {
+    //BackGround and textColor for both light modes
     const bg = useColorModeValue("white", "gray.800");
+    const textColor = useColorModeValue('gray.500', 'gray.200')
+
+    //Extracting deleteProduct from global state hook
+    const {deleteProduct} = useProductStore()
+
+    //Getting toast (pop-up notifications from chakra)
+    const toast = useToast();
+
+	const handleDeleteProduct = async (productId) => {
+		const { success, message } = await deleteProduct(productId);
+		if (success) {
+			toast({
+				title: "Error",
+				description: message,
+				status: "error",
+				duration: 3000,
+				isClosable: true,
+			});
+		} else {
+			toast({
+				title: "Success",
+				description: message,
+				status: "success",
+				duration: 3000,
+				isClosable: true,
+			});
+		}
+	};
+
+
+
+
     return (
         <Box
             shadow="lg"
@@ -11,7 +45,27 @@ const ProductCard = ({ product }) => {
             _hover={{ transform: "translateY(-5px)", shadow: "xl" }}
             bg={bg}
         >
-            {/* Add content for the product card here */}
+        <Image src={product.image} alt={product.name} h={48} w="full" objectFit='cover' />
+        <Box p={4}>
+            <Heading as='h3' size='md' mb={2}>
+				{product.name}
+			</Heading>
+        
+        <Text fontWeight='bold' fontSize='xl' color={textColor} mb={4}>
+			${product.price}
+		</Text>
+
+        <HStack spacing={2}>
+			<IconButton icon={<EditIcon />}  colorScheme='blue' />
+			<IconButton
+				icon={<DeleteIcon />}
+                onClick={() => handleDeleteProduct(product._id)}
+				colorScheme='red'
+			/>
+		</HStack>
+        
+
+        </Box>
         </Box>
     );
 };
--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -5,6 +5,7 @@ export const useProductStore = create((set) => ({
 	products: [],
 	//Populates the products array
 	setProducts: (products) => set({ products }),
+
 	//Verifies if all fields and filled and either creates a new product or returns an error message
 	createProduct: async (newProduct) => {
 		if (!newProduct.name || !newProduct.image || !newProduct.price) {
@@ -21,10 +22,24 @@ export const useProductStore = create((set) => ({
 		set((state) => ({ products: [...state.products, data.data] }));
 		return { success: true, message: "Product created successfully" };
 	},
+
 	//Fetches all products
 	getProducts: async () => {
 		const res = await fetch("http://localhost:5000/products")
 		const data = await res.json()
 		set({products: data.data})
+	},
+	
+	//Delete product
+	deleteProduct: async (productId) => {
+		const res = await fetch(`http://localhost:5000/products/${productId}`, {
+			method: "DELETE",
+		});
+		const data = await res.json();
+		if (!data.success) return { success: false, message: data.message };
+
+		set((state) => ({ products: state.products.filter((product) => product._id !== productId) }));
+		return { success: true, message: data.message };
 	}
+
 }));
