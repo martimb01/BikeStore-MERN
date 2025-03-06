8c874d35b2e72a869071de0b38c0499ab04eadeb
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-03-06 15:03:07 +0000

    Added getProducts function to our global state hook and working on HomePage component

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -1,9 +1,11 @@
 import { create } from "zustand";
 
-// Using this as a global state hook
+// Global state hook
 export const useProductStore = create((set) => ({
 	products: [],
+	//Populates the products array
 	setProducts: (products) => set({ products }),
+	//Verifies if all fields and filled and either creates a new product or returns an error message
 	createProduct: async (newProduct) => {
 		if (!newProduct.name || !newProduct.image || !newProduct.price) {
 			return { success: false, message: "Please fill in all fields." };
@@ -18,5 +20,11 @@ export const useProductStore = create((set) => ({
 		const data = await res.json();
 		set((state) => ({ products: [...state.products, data.data] }));
 		return { success: true, message: "Product created successfully" };
+	},
+	//Fetches all products
+	getProducts: async () => {
+		const res = await fetch("http://localhost:5000/products")
+		const data = await res.json()
+		set({products: data.data})
 	}
 }));
